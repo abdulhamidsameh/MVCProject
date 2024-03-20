ca2153af25490fc1cf0d4946e7b42a5695970bbd
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-03-20 07:33:28 +0200

    Part 03 3-Layers Arch. Pattern - Data Access Layer

---
+++ MVCProject.DAL/Data/MVCProjectDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCProject.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProject.DAL.Data
+{
+    internal class MVCProjectDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server = .;Database=MVCProject;Trusted_Connection=True");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+            => modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        public DbSet<Department> Departments { get; set; }
+    }
+}
