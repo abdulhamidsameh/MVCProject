cc40fb94508e629f966bc38ba8631aa92ed990a4
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-03-23 08:59:30 +0200

    Part 02 Department Controller - Edit

--- MVCProject.PL/Controllers/DepartmentController.cs
+++ MVCProject.PL/Controllers/DepartmentController.cs
@@ -1,22 +1,28 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.Hosting;
 using MVCProject.BLL.Interfaces;
 using MVCProject.DAL.Models;
+using System;
 namespace MVCProject.PL.Controllers
 {
     public class DepartmentController : Controller
     {
         private readonly IDepartmentRepository _departmentRepo;
+        private readonly IWebHostEnvironment _env;
 
-        public DepartmentController(IDepartmentRepository departmentRepo)
+        public DepartmentController(IDepartmentRepository departmentRepo, IWebHostEnvironment env)
         {
             _departmentRepo = departmentRepo;
+            _env = env;
         }
         public IActionResult Index()
         {
             var departments = _departmentRepo.GetAll();
             return View(departments);
         }
-        [HttpGet]
+        //[HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -34,16 +40,42 @@ namespace MVCProject.PL.Controllers
             return View(department);
         }
         // /Department/Details/id
-        [HttpGet]
-        public IActionResult Details(int? id) 
+        //[HttpGet]
+        public IActionResult Details(int? id,string ViewName="Details") 
         {
             if(!id.HasValue)
                 return BadRequest();
             var department = _departmentRepo.Get(id.Value);
             if(department is null)
                 return NotFound();
-
-            return View(department);
+            return View(ViewName,department);
+        }
+        // /Department/Edit/10
+        //[HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            return Details(id,"Edit");
+        }
+        [HttpPost]
+        public IActionResult Edit([FromRoute] int id,Department department)
+        {
+            if(id != department.Id)
+                return BadRequest();
+            if(!ModelState.IsValid)
+                return View(department);
+            try
+            {
+                _departmentRepo.Update(department);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                if(_env.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                else
+                    ModelState.AddModelError(string.Empty, "An Error Has Occured during Updating the Department");
+                return View(department);
+            } 
         }
     }
 }
