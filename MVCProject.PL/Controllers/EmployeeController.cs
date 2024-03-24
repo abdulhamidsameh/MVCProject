5405fcca4e76d60c3b25e367e58f75c30b2d89d2
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-03-24 06:05:39 +0200

    Part 08 Employee Controller

---
+++ MVCProject.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using MVCProject.BLL.Repositories;
+using MVCProject.DAL.Models;
+using System;
+
+namespace MVCProject.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly EmployeeRepository _employeeRepo;
+        private readonly IWebHostEnvironment _env;
+
+        public EmployeeController(EmployeeRepository employeeRepo, IWebHostEnvironment env)
+        {
+            _employeeRepo = employeeRepo;
+            _env = env;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var employees = _employeeRepo.GetAll();
+            return View(employees);
+        }
+        [HttpGet]
+        public IActionResult Create() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            if(ModelState.IsValid) 
+            {
+                var Count = _employeeRepo.Add(employee);
+                if(Count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(employee);
+        }
+        [HttpGet]
+        public IActionResult Deteils(int? id,string ViewName ="Details") 
+        {
+            if (!id.HasValue)
+                return BadRequest();
+            var employee = _employeeRepo.Get(id.Value);
+            if(employee is null)
+                return NotFound();
+            return View(ViewName, employee);
+        }
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            return Deteils(id,"Edit");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id,Employee employee)
+        {
+            if(id != employee.Id)
+                return BadRequest();
+            if(!ModelState.IsValid)
+                return View(employee);
+            try
+            {
+                _employeeRepo.Update(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                if (_env.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                else
+                    ModelState.AddModelError(string.Empty, "An Error Has Occured during Updating the Employee");
+                return View(employee);
+            }
+        }
+        [HttpGet]
+        public IActionResult Delete(int? id) 
+        {
+            return Deteils(id,"Delete");
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Employee employee)
+        {
+            try
+            {
+                _employeeRepo.Delete(employee);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                if (_env.IsDevelopment())
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                else
+                    ModelState.AddModelError(string.Empty, "An Error Has Occured during Deleting the Employee");
+                return View(employee);
+            }
+        }
+    }
+}
