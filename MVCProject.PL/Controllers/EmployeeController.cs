f721d01ad248aa2d67289e3544d35fb55dd576c1
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-03-27 05:49:20 +0200

    Part 03 TempData

--- MVCProject.PL/Controllers/EmployeeController.cs
+++ MVCProject.PL/Controllers/EmployeeController.cs
@@ -8,99 +8,100 @@ using System;
 
 namespace MVCProject.PL.Controllers
 {
-    public class EmployeeController : Controller
-    {
-        private readonly IEmployeeRepository _employeeRepo;
-        private readonly IWebHostEnvironment _env;
+	public class EmployeeController : Controller
+	{
+		private readonly IEmployeeRepository _employeeRepo;
+		private readonly IWebHostEnvironment _env;
 
-        public EmployeeController(IEmployeeRepository employeeRepo, IWebHostEnvironment env)
-        {
-            _employeeRepo = employeeRepo;
-            _env = env;
-        }
-        [HttpGet]
-        public IActionResult Index()
-        {
-            var employees = _employeeRepo.GetAll();
-            return View(employees);
-        }
-        [HttpGet]
-        public IActionResult Create() 
-        {
-            return View();
-        }
-        [HttpPost]
-        public IActionResult Create(Employee employee)
-        {
-            if(ModelState.IsValid) 
-            {
-                var Count = _employeeRepo.Add(employee);
-                if(Count > 0)
-                {
-                    return RedirectToAction(nameof(Index));
-                }
-            }
-            return View(employee);
-        }
-        [HttpGet]
-        public IActionResult Details(int? id,string ViewName ="Details") 
-        {
-            if (!id.HasValue)
-                return BadRequest();
-            var employee = _employeeRepo.Get(id.Value);
-            if(employee is null)
-                return NotFound();
-            return View(ViewName, employee);
-        }
-        [HttpGet]
-        public IActionResult Edit(int? id)
-        {
-            return Details(id,"Edit");
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit([FromRoute] int id,Employee employee)
-        {
-            if(id != employee.Id)
-                return BadRequest();
-            if(!ModelState.IsValid)
-                return View(employee);
-            try
-            {
-                _employeeRepo.Update(employee);
-                return RedirectToAction(nameof(Index));
-            }
-            catch (Exception ex)
-            {
-                if (_env.IsDevelopment())
-                    ModelState.AddModelError(string.Empty, ex.Message);
-                else
-                    ModelState.AddModelError(string.Empty, "An Error Has Occured during Updating the Employee");
-                return View(employee);
-            }
-        }
-        [HttpGet]
-        public IActionResult Delete(int? id) 
-        {
-            return Details(id,"Delete");
-        }
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Delete(Employee employee)
-        {
-            try
-            {
-                _employeeRepo.Delete(employee);
-                return RedirectToAction(nameof(Index));
-            }
-            catch (Exception ex)
-            {
-                if (_env.IsDevelopment())
-                    ModelState.AddModelError(string.Empty, ex.Message);
-                else
-                    ModelState.AddModelError(string.Empty, "An Error Has Occured during Deleting the Employee");
-                return View(employee);
-            }
-        }
-    }
+		public EmployeeController(IEmployeeRepository employeeRepo, IWebHostEnvironment env)
+		{
+			_employeeRepo = employeeRepo;
+			_env = env;
+		}
+		[HttpGet]
+		public IActionResult Index()
+		{
+			var employees = _employeeRepo.GetAll();
+			return View(employees);
+		}
+		[HttpGet]
+		public IActionResult Create()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult Create(Employee employee)
+		{
+			if (ModelState.IsValid)
+			{
+				var Count = _employeeRepo.Add(employee);
+				if (Count > 0)
+					TempData["Succes"] = "Employee Is Created Successfuly";
+				else
+					TempData["Fail"] = "An Error Has Occured, Employee Not Created :(";
+				return RedirectToAction(nameof(Index));
+			}
+			return View(employee);
+		}
+		[HttpGet]
+		public IActionResult Details(int? id, string ViewName = "Details")
+		{
+			if (!id.HasValue)
+				return BadRequest();
+			var employee = _employeeRepo.Get(id.Value);
+			if (employee is null)
+				return NotFound();
+			return View(ViewName, employee);
+		}
+		[HttpGet]
+		public IActionResult Edit(int? id)
+		{
+			return Details(id, "Edit");
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit([FromRoute] int id, Employee employee)
+		{
+			if (id != employee.Id)
+				return BadRequest();
+			if (!ModelState.IsValid)
+				return View(employee);
+			try
+			{
+				_employeeRepo.Update(employee);
+				return RedirectToAction(nameof(Index));
+			}
+			catch (Exception ex)
+			{
+				if (_env.IsDevelopment())
+					ModelState.AddModelError(string.Empty, ex.Message);
+				else
+					ModelState.AddModelError(string.Empty, "An Error Has Occured during Updating the Employee");
+				return View(employee);
+			}
+		}
+		[HttpGet]
+		public IActionResult Delete(int? id)
+		{
+			return Details(id, "Delete");
+		}
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Delete(Employee employee)
+		{
+			try
+			{
+				_employeeRepo.Delete(employee);
+				return RedirectToAction(nameof(Index));
+			}
+			catch (Exception ex)
+			{
+				if (_env.IsDevelopment())
+					ModelState.AddModelError(string.Empty, ex.Message);
+				else
+					ModelState.AddModelError(string.Empty, "An Error Has Occured during Deleting the Employee");
+				return View(employee);
+			}
+		}
+	}
 }
